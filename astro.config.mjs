14f60855c93bea997b8f72fa2169264c0cba777a
Paul <97813342+Paul1404@users.noreply.github.com>
2023-09-03 18:05:43 +0200

    Revert "Cloudflare SSR"
    This reverts commit 2553f83a9650d454fa6076a8c311bca3b72c1df0.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config';
 import { Application } from '@splinetool/runtime';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare()
-});
+export default defineConfig({});
