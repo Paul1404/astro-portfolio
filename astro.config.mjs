2553f83a9650d454fa6076a8c311bca3b72c1df0
Paul <97813342+Paul1404@users.noreply.github.com>
2023-09-03 02:16:39 +0200

    Cloudflare SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
 import { Application } from '@splinetool/runtime';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare()
+});
