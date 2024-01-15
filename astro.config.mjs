836f05ccbdfe79e14c2d6a611f9b7a8707b4441a
Paul <97813342+Paul1404@users.noreply.github.com>
2024-01-15 21:09:11 +0100

    Added robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pd-portfolio.net/',
-  integrations: [sitemap()]
+  integrations: [sitemap(), robotsTxt()]
 });
